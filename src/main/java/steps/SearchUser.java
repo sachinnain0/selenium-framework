9aa443a55fb9f63abdd76ccf0d47f7f0a7160db4
Sachin <Sachin@192.168.1.5>
2018-11-11 20:50:56 +0530

    Final Commit

--- src/main/java/steps/SearchUser.java
+++ src/main/java/steps/SearchUser.java
@@ -3,7 +3,6 @@ package steps;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
@@ -44,8 +43,9 @@ public void validates_if_user_exists_in_HRM_portal(DataTable dataTable) throws T
 	List<WebElement> resultSetUserName = sysUsers.labelUserNameResultTable;
 	
 	if(resultSetUserName.size()==1){
-		Assert.assertEquals(data.get(0).get("SearchUserName"), resultSetUserName.get(0).getText());
-		
+		if(data.get(0).get("SearchUserName").equalsIgnoreCase(resultSetUserName.get(0).getText())) {
+			throw new Exception("Incorrect search result- Expected: "+data.get(0).get("SearchUserName")+" but Actaul: "+resultSetUserName.get(0).getText());
+		}
 	}
 	ScreenshotsManagement.takeScreenshot();
 }
